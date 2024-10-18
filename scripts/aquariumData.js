608315e08ffd4c1cf311aef1ab7579e9e68c3e2e
William R. Stetar <kebekad673@proton.me>
2024-10-18 18:45:27 -0500

    Initial Commit

---
+++ scripts/aquariumData.js
@@ -0,0 +1,62 @@
+// scripts/aquariumData.js
+
+export const database = {
+  fish: [
+    {
+      name: "Nemo",
+      species: "Clownfish",
+      length: 3,
+      location: "Great Barrier Reef",
+      diet: "Omnivore",
+      image: "https://example.com/images/nemo.jpg"
+    },
+    {
+      name: "Dory",
+      species: "Blue Tang",
+      length: 5,
+      location: "Great Barrier Reef",
+      diet: "Omnivore",
+      image: "https://example.com/images/dory.jpg"
+    },
+    {
+      name: "Bruce",
+      species: "Great White Shark",
+      length: 15,
+      location: "Coastal waters of Australia",
+      diet: "Carnivore",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Great_white_shark.jpg/800px-Great_white_shark.jpg"
+    },
+    {
+      name: "Marlin",
+      species: "Regal Blue Tang",
+      length: 6,
+      location: "Caribbean Sea",
+      diet: "Omnivore",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Paracanthurus_hepatus_1.jpg/800px-Paracanthurus_hepatus_1.jpg"
+    },
+    {
+      name: "Gill",
+      species: "Moorish Idol",
+      length: 4,
+      location: "Tropical waters of the Atlantic and Pacific",
+      diet: "Omnivore",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Moorish_Idol.jpg/800px-Moorish_Idol.jpg"
+    },
+    {
+      name: "Bloat",
+      species: "Porcupine Pufferfish",
+      length: 8,
+      location: "Tropical waters of the Atlantic and Pacific",
+      diet: "Omnivore",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Porcupine_Pufferfish.jpg/800px-Porcupine_Pufferfish.jpg"
+    },
+    {
+      name: "Peach",
+      species: "Starfish",
+      length: 2,
+      location: "Tropical waters of the Atlantic and Pacific",
+      diet: "Detritivore",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Starfish.jpg/800px-Starfish.jpg"
+    }
+  ]
+}
---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+import { fishList } from './fishList.js'
+import { tipList } from './tipList.js'
+import { locationList } from './locationList.js'
+
+// Generate the fish list
+const fishHTML = fishList()
+
+// Generate the care tips
+const tipHTML = tipList()
+
+// Generate the location list
+const locationHTML = locationList()
+
+// Render each HTML string to the correct DOM element
