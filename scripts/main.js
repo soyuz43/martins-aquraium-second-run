63fd9e1f1f2e7ac92762e39a3e154c04dbc89721
William R. Stetar <kebekad673@proton.me>
2024-10-23 15:58:56 -0500

    Data Rendering to the DOM

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+// scripts/main.js
 import { fishList } from './fishList.js'
 import { tipList } from './tipList.js'
 import { locationList } from './locationList.js'
@@ -11,4 +12,12 @@ const tipHTML = tipList()
 // Generate the location list
 const locationHTML = locationList()
 
+// Get the DOM elements where you want to render the HTML
+const fishContainer = document.getElementById('fishList')
+const tipContainer = document.getElementById('tipList')
+const locationContainer = document.getElementById('locationList')
+
 // Render each HTML string to the correct DOM element
+fishContainer.innerHTML = fishHTML
+tipContainer.innerHTML = tipHTML
+locationContainer.innerHTML = locationHTML
